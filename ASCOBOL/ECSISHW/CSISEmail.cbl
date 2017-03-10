553e3a4ef36a4cb467497648e1b78214da921e28
Aaron Navarro <aaronnavarro.112@gmail.com>
2017-03-10 10:38:40 +0800

    Added Country Codes and SortEmailDomain

--- ASCOBOL/ECSISHW/CSISEmail.cbl
+++ ASCOBOL/ECSISHW/CSISEmail.cbl
@@ -19,8 +19,16 @@
          02 GradYear            PIC 9(4).
          02 CourseCode          PIC 9(1).
          02 EmailAdd            PIC X(28).
-         02 EmailDomainName     PIC X(20).
 
-       FD Country.
-       01 CountryCode         PIC X(2).
-       01 CountryName X(26).
+         01 CountryName         PIC X(26).
+
+       SD CountryCodes.
+       01 CountryCode           PIC X(2).
+       01 CCCountryName         PIC X(26).
+
+       FD SortDomain.
+       01 SDEmailDomainName     PIC X(20).
+       01 SDStudentName         PIC X(25).
+       01 SDGradYear              PIC 9(4).
+       01 SDCourseName          PIC X(25).
+       01 SDCountryName         PIC X(26).
